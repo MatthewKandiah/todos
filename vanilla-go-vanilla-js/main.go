9f67e7602d22f77a060033b0364527eb36cc1281
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-06-19 21:47:35 +0100

    post us a name, we'll fetch your TODOs from the "database" - still need to work out how to iterate a list in go template though

--- vanilla-go-vanilla-js/main.go
+++ vanilla-go-vanilla-js/main.go
@@ -9,33 +9,62 @@ import (
 )
 
 func main() {
+	// let's just pretend this is a database!
+	db := make(map[string][]Todo)
+	todo := Todo{description: "Buy milk"}
+	db["bob"] = append(db["bob"], todo)
+
 	r := mux.NewRouter()
 
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "<a href=\"https:localhost:8913/books/moby-dick/page/456\">Wanna hear about Moby Dick?</a>")
+		t := template.Must(template.New("loginTemplate").Parse(loginTemplate))
+		t.Execute(w, nil)
 	})
 
-	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/user/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
-		title := vars["title"]
-		page := vars["page"]
-		data := TemplateData{
-			BookName: title,
-			Page:     page,
+		name := vars["name"]
+		todos := db[name]
+		data := UserTodosData{
+			Name:  name,
+			Todos: todos,
 		}
-		t := template.Must(template.ParseFiles("template.html"))
-		fmt.Fprintf(w, "<!DOCTYPE html><html lang=\"en\"><head></head><body>")
-		fmt.Fprintf(w, "<h4>Template contents start...</h4>")
+		t := template.Must(template.New("userTodosTemplate").Parse(userTodosTemplate))
 		t.Execute(w, data)
-		fmt.Fprintf(w, "<h4>Template contents finished!</h4>")
-		fmt.Fprintf(w, "</body></html>")
-	})
+	}).Methods("GET").Schemes("http")
+
+	r.HandleFunc("/user/login", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		redirectUrl := fmt.Sprintf("/user/%s", r.Form["loginName"][0])
+		http.Redirect(w, r, redirectUrl, http.StatusSeeOther)
+	}).Methods("POST").Schemes("http")
 
 	fmt.Println("Starting server")
 	http.ListenAndServe(":8913", r)
 }
 
-type TemplateData struct {
-	BookName string
-	Page     string
+type Todo struct {
+	description string
 }
+
+type UserTodosData struct {
+	Name  string
+	Todos []Todo
+}
+
+const loginTemplate = `
+<h1>Login</h1>
+<form action="/user/login" method="post">
+  <label for="loginName"></label>
+  <input type="text" id="loginName" name="loginName"></input>
+  <br/>
+  <input type="submit" value="Submit"/>
+</form>
+	`
+
+const userTodosTemplate = `
+<h1>{{.Name}}</h1>
+<ul>
+  <li>{{.Todos}}</li>
+</ul>
+	`
