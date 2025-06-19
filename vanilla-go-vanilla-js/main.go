ceb0b2baccfda962ea7dfe88610d81675bd5dce2
Matthew Kandiah <mj.kandiah@googlemail.com>
2025-06-19 08:55:44 +0100

    example pulling values off a url and injecting them into a template

--- vanilla-go-vanilla-js/main.go
+++ vanilla-go-vanilla-js/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -10,14 +11,31 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<a href=\"https:localhost:8913/books/moby-dick/page/456\">Wanna hear about Moby Dick?</a>")
+	})
+
 	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
 		title := vars["title"]
 		page := vars["page"]
-
-		fmt.Fprintf(w, "You requested the book: %s on page %s\n", title, page)
+		data := TemplateData{
+			BookName: title,
+			Page:     page,
+		}
+		t := template.Must(template.ParseFiles("template.html"))
+		fmt.Fprintf(w, "<!DOCTYPE html><html lang=\"en\"><head></head><body>")
+		fmt.Fprintf(w, "<h4>Template contents start...</h4>")
+		t.Execute(w, data)
+		fmt.Fprintf(w, "<h4>Template contents finished!</h4>")
+		fmt.Fprintf(w, "</body></html>")
 	})
 
 	fmt.Println("Starting server")
 	http.ListenAndServe(":8913", r)
+}
+
+type TemplateData struct {
+	BookName string
+	Page     string
 }
